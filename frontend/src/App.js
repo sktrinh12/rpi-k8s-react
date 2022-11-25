b721219d4b0ed831e6f30c188e88f60fbdee0c72
sktrinh12 <sktrinh12@gmail.com>
2022-11-24 19:35:34 -0800

    functions to handle click evts and state mgmt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,77 @@
+import React, { useEffect, useRef, useState } from "react";
 import LCDdisplay from "./LCDdisplay";
+import Title from "./Title";
+import Input from "./Input";
 import "./App.css";
 
 function App() {
+  const [stringTop, setStringTop] = useState("ABCDEFGHIJKLMNOP");
+  const [stringBottom, setStringBottom] = useState("1234567890ABCDEF");
+  const stringTopRef = useRef(null);
+  const stringBottomRef = useRef(null);
+  const submitStringsRef = useRef(null);
+
+  useEffect(() => {
+    stringTopRef.current.focus();
+  }, []);
+
+  const stringTopOnEnter = (e) => {
+    if (e.key === "Enter") {
+      stringBottomRef.current.focus();
+    }
+  };
+
+  const stringTopChange = (e) => {
+    e.preventDefault();
+    setStringTop(e.target.value);
+  };
+
+  const stringBottomChange = (e) => {
+    e.preventDefault();
+    setStringBottom(e.target.value);
+  };
+
+  const svgOnClick = () => {
+    stringTopRef.current.focus();
+  };
+
+  const onClickSubmit = (e) => {
+    console.log(
+      `Form submitted: ${stringTop} <-> ${stringTopRef.current.value} & ${stringBottom} <-> ${stringBottomRef.current.value}`
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <LCDdisplay className="lcd-class" />
+        <Title
+          className="header-style"
+          title="Raspberry Pi LCD Display"
+        ></Title>
+        <Input
+          type="text"
+          onChange={stringTopChange}
+          onKeyDown={stringTopOnEnter}
+          ref={stringTopRef}
+        />
+        <Input
+          type="text"
+          onChange={stringBottomChange}
+          ref={stringBottomRef}
+        />
+        <LCDdisplay
+          className="lcd-class"
+          stringTop={stringTop}
+          stringBottom={stringBottom}
+          onClick={svgOnClick}
+        />
+        <button
+          className="button-style"
+          onClick={onClickSubmit}
+          ref={submitStringsRef}
+        >
+          Submit
+        </button>
         <p>Type in text to see it appear on the LCD display!</p>
       </header>
     </div>
