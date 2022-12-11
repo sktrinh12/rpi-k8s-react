7888eece7afaeec25372d8728e92c44c8086df0d
Spencer <sktrinh12@gmail.com>
2022-12-11 07:26:28 +0000

    add Query/Path params to endpoints, change name of some vars

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Query, Path
+from fastapi.exceptions import HTTPException
 from app.RPi_I2C_driver import *
 from os import getenv
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,8 +11,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "localhost:3000",
-    "http://192.168.1.12",
-    "http://k8s-main",
+    "http://192.168.1.12", # linux laptop fixed-ip
+    "http://k8s-main", # resolv.conf
 ]
 
 app.add_middleware(
@@ -22,21 +23,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-dct = {}
+env_dct = {}
 lcd_class = lcd()
 lcd_class.lcd_clear()
 freezetime = freeze_time(datetime.now(), tick=True)
 freezetime.start()
+response = requests.get(TIME_API)
+response.close()
+timezones = response.json()
+clock_types = ['bcd', 'binary']
 
 @app.on_event("startup")
 async def startup_event():
-    dct["NODE_NAME"] = getenv("NODE_NAME", "NODE_NAME")
-    dct["POD_NAMESPACE"] = getenv("POD_NAMESPACE", "POD_NAMESPACE")
-    dct["POD_NAME"] = getenv("POD_NAME", "POD_NAME")
-    dct["POD_IP"] = getenv("POD_IP", "POD_IP")
-    dct["POD_SVC"] = getenv("POD_SVC", "POD_SVC")
-    if len(dct) % 2 != 0:
-        dct["PHANTOM"] = ""
+    global env_dct
+    env_dct["NODE_NAME"] = getenv("NODE_NAME", "NODE_NAME")
+    env_dct["POD_NAMESPACE"] = getenv("POD_NAMESPACE", "POD_NAMESPACE")
+    env_dct["POD_NAME"] = getenv("POD_NAME", "POD_NAME")
+    env_dct["POD_IP"] = getenv("POD_IP", "POD_IP")
+    env_dct["POD_SVC"] = getenv("POD_SVC", "POD_SVC")
+    if len(env_dct) % 2 != 0:
+        env_dct["PHANTOM"] = ""
 
 
 @app.on_event("shutdown")
@@ -46,39 +52,46 @@ def shutdown_event():
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     welcome = {"Hello": "Raspberry Pi with LCD display!"}
-    return {**welcome, **dct}
+    return {**welcome, **env_dct}
 
 
 @app.get("/info")
-def info():
-    res = get_info()
+async def info():
+    res = get_info(lcd_class, env_dct)
     rtn = dict(STATUS_CODE=status.HTTP_200_OK)
-    return {**dct, **rtn}
+    return {**env_dct, **rtn}
 
 
 @app.get("/lcd")
-async def lcd(string_top: str, string_bottom: str, delay: int):
+async def lcd(string_top: str = Query(min_length=1, max_length=16), string_bottom: str = Query(min_length=1, max_length=16), elapse: int = Path(title="Time elapsed for displaying on lcd screen", ge=0)):
     lcd_class.lcd_clear()
     lcd_class.lcd_display_string(string_top.upper(), 1)
     lcd_class.lcd_display_string(string_bottom.upper(), 2)
-    if delay > 0:
-        sleep(int(delay))
+    if elapse > 0:
+        sleep(int(elapse))
         lcd_class.lcd_clear()
-    res = {"MSG": f"Dispalyed: {string_top} & {string_bottom}, delay of: {delay}"}
+    res = {"MSG": f"Dispalyed: {string_top} & {string_bottom}, elapse time: {elapse}"}
     rtn = dict(STATUS_CODE=status.HTTP_200_OK)
     return {**res, **rtn}
 
 @app.get("/binary-clock")
-async def binary_clock(tzone: str):
-    output_dct = sync_time(tzone, freezetime)
-    time_tuple = output_dct['TIME_TUPLE']
-    hour = time_tuple[0]
-    minute = time_tuple[1]
-    seconds = time_tuple[2]
-    data = binary_output((hour, minute, seconds))
-    data.update(output_dct)
+async def binary_clock(tzone: str = Query(...), clock_type: str = Query(min_length=3, max_length=6)):
+    if tzone not in timezones:
+        raise HTTPException( status_code=409, detail=f"That timezone does not exist, {tzone}")
+    if clock_type not in clock_types:
+        raise HTTPException( status_code=409, detail=f"Clock-type must be either {' or '.join(clock_types)}, {clock_type} is invalid")
+    output_datetime_dct = sync_time(tzone, freezetime)
+    time_tuple = output_datetime_dct['TIME_TUPLE']
+    hour = time_tuple[3]
+    minute = time_tuple[4]
+    seconds = time_tuple[5]
+    if clock_type == 'binary':
+        data = binary_output((hour, minute, seconds), unit_time_dct[clock_type])
+    elif clock_type == 'bcd':
+        data = bcd_output((hour, minute, seconds))
+    data.update(output_datetime_dct)
     # display_leds(data)
     return data
 
@@ -88,7 +101,4 @@ async def current_time():
 
 @app.get("/tzones")
 async def time_zones():
-    response = requests.get(TIME_API)
-    response.close()
-    json = response.json()
-    return json
+    return timezones 
