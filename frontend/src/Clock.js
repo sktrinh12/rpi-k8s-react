48eeb9c1ecc9dd431075156add0f22abfd4a26de
sktrinh12 <sktrinh12@gmail.com>
2022-12-18 02:26:47 -0800

    use state to fetch api and render Screen

--- frontend/src/Clock.js
+++ frontend/src/Clock.js
@@ -1,6 +1,5 @@
-import React from "react";
-import Led from "./Led";
-import Screen from "./Screen";
+import React, { useEffect, useState } from "react";
+import ScreenTwo from "./ScreenTwo";
 
 const range = (start, end, step) => {
   return Array.from(
@@ -8,50 +7,60 @@ const range = (start, end, step) => {
     (e, x) => start + x * step
   );
 };
-const colours = {
-  blue: ["#005dc3", "#0066c3", "#aec5ff"],
-  red: ["#a30000", "#8e0000", "#f3c9c9"],
-  green: ["#275214", "#214611", "#c3d5bb"],
-};
+
+const baseURL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8000";
+const yStart = 15;
+const yStep = 65;
+const xStep = 75;
 
 const Clock = () => {
-  const numberOfCircles = 56;
-  const arrayOfCircles = Array.from({ length: 8 }, (x) =>
-    range(1, Math.floor(numberOfCircles / 8), 1)
-  );
-  // console.log(arrayOfCircles);
+  const clockType = "binary";
+  const tzone = "Asia/Ho_Chi_Minh";
+  const [data, setData] = useState({});
+  // const timeData = Object.keys(data)
+  //   .filter((key) => /^H|^M|^S/.test(key))
+  //   .reduce((obj, key) => {
+  //     return Object.assign(obj, {
+  //       [key]: data[key],
+  //     });
+  //   }, {});
+  const xStart = data.length === 3 ? 275 : 55;
+  const numRows = data.length === 3 ? 4 : 8;
+  const numColumns = data.length === 3 ? 3 : 6;
+  const arrayRows = range(xStart, xStart + numColumns * xStart, xStep);
+  const arrayColumns = range(yStart, yStart + numRows * yStart, yStep);
+  // console.log(data);
+
+  let getTime = async (changeTime = true) => {
+    try {
+      const backendURL = [
+        baseURL,
+        changeTime
+          ? `/binary-clock?clock_type=${clockType}&tzone=${tzone}`
+          : `/ctime?clock_type=${clockType}`,
+      ].join("");
+      // console.log(backendURL);
+      let fetched = await fetch(backendURL);
+      if (fetched) {
+        let json = await fetched.json();
+        // console.log(json);
+        setData(json);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTime();
+    let everySecond = setInterval(() => getTime(false), 5000);
+    return () => {
+      clearInterval(everySecond);
+    };
+  }, []);
 
   return (
-    <table>
-      <tbody>
-        {arrayOfCircles.map((arrOut, i) => {
-          return (
-            <React.Fragment key={`frag_${i}`}>
-              <tr key={`row_${i}`}>
-                {arrOut.map((arrIn, j) => {
-                  return (
-                    <>
-                      <td key={`cell_${i}${j}`}>
-                        {j === 0 && i === 0 && (
-                          <Screen className="inner_wrap" />
-                        )}
-                        <Led
-                          key={`led_${i}${j}`}
-                          colours={
-                            j % 2 === 0 ? colours["green"] : colours["blue"]
-                          }
-                          radColourIds={`${i}${j}`}
-                        />
-                      </td>
-                    </>
-                  );
-                })}
-              </tr>
-            </React.Fragment>
-          );
-        })}
-      </tbody>
-    </table>
+    <ScreenTwo data={data} arrayColumns={arrayColumns} arrayRows={arrayRows} />
   );
 };
 
