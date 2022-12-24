405d94e4b362cb63add757cf8927f762b8991e86
sktrinh12 <sktrinh12@gmail.com>
2022-12-24 15:06:04 -0800

    only timdata passed as props, render text in svg

--- frontend/src/Screen.js
+++ frontend/src/Screen.js
@@ -10,29 +10,93 @@ const colours = {
 
 const r = 4.25;
 
-const Screen = ({ bitsData, timeData, arrayColumns, arrayRows }) => {
+const range = (start, end, step) => {
+  return Array.from(
+    { length: Math.ceil((end - start) / step) },
+    (e, x) => start + x * step
+  );
+};
+
+const regexFilter = (pattern, json) => {
+  const regex = new RegExp(pattern);
+  return Object.keys(json)
+    .filter((key) => regex.test(key))
+    .reduce((obj, key) => {
+      return Object.assign(obj, {
+        [key]: json[key],
+      });
+    }, {});
+};
+
+const returnTimeDigits = (tData, dataLength) => {
+  let timeDigits;
+  if (dataLength === 3) {
+    // if bitsData contains 3 elements for 'binary' - H:M:S
+    timeDigits = [tData.hour, tData.minute, tData.seconds];
+  } else {
+    // if bitsData contains 6 elements for 'bcd' - HH:MM:SS
+    timeDigits = [
+      ...tData.hour.split(""),
+      ...tData.minute.split(""),
+      ...tData.seconds.split(""),
+    ];
+  }
+  return timeDigits;
+};
+
+const Screen = ({ timeData }) => {
+  const tData = regexFilter("^hour|^minute|^seconds", timeData);
+  const bData = regexFilter("^H|^M|^S", timeData);
+  const dataLength = Object.keys(bData).length;
+  const timeDigits = returnTimeDigits(tData, dataLength);
+  // console.log(timeData);
+  // console.log(dataLength);
+  const width = 100;
+  const height = 88;
+
+  // if bcd is selected need 6 columns (keys) HH:MM:SS
+  let numCirclesX = 6;
+  let numCirclesY = 4;
+  // if binary is selected only need three columns (keys) H:M:S
+  if (dataLength === 3) {
+    numCirclesX = 3;
+    numCirclesY = 7;
+  }
+  const XSpacing = width / (numCirclesX + 1);
+  const YSpacing = height / (numCirclesY + 1);
+  const arrayColumns = range(
+    YSpacing,
+    YSpacing + numCirclesY * YSpacing,
+    YSpacing
+  );
+  const arrayRows = range(
+    XSpacing,
+    XSpacing + numCirclesX * XSpacing,
+    XSpacing
+  );
+  // console.log(arrayColumns);
+  // console.log(arrayRows);
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       version="1.1"
-      viewBox="0 0 100 80"
+      viewBox="0 0 100 100"
     >
       <defs>
         <linearGradient id="linearGradient1582">
-          <stop stopColor="#191919" stopOpacity="1" offset="0" id="stop1578" />
-          <stop stopColor="#323232" stopOpacity="1" offset="1" id="stop1580" />
+          <stop stopColor="#191919" stopOpacity="0.95" offset="0" />
+          <stop stopColor="#323232" stopOpacity="1" offset="1" />
         </linearGradient>
         <radialGradient
           xlinkHref="#linearGradient1582"
           id="radialGradient1576"
-          cx="75%"
-          cy="75%"
-          fx="75%"
-          fy="75%"
-          r="450%"
-          gradientTransform="matrix(1.1182301,-0.00953598,0.00835003,0.97916037,-236.85132,-183.66633)"
-          gradientUnits="userSpaceOnUse"
+          cx="50%"
+          cy="50%"
+          fx="50%"
+          fy="50%"
+          r="50%"
         />
         {/* ELLIPSE GRADIENT FOR LEDS */}
         <radialGradient
@@ -53,7 +117,7 @@ const Screen = ({ bitsData, timeData, arrayColumns, arrayRows }) => {
         rx="9"
         ry="9"
         width="98%"
-        height="96%"
+        height="97%"
         fill="url(#radialGradient1576)"
         fillOpacity="1"
         stroke="#1b1b1b"
@@ -61,7 +125,7 @@ const Screen = ({ bitsData, timeData, arrayColumns, arrayRows }) => {
       />
       <>
         {/* bit data */}
-        {Object.values(bitsData).map((bits, i) => {
+        {Object.values(bData).map((bits, i) => {
           // 3 or 6 keys
           return (
             <React.Fragment key={`bit_frag_${i}`}>
@@ -70,7 +134,7 @@ const Screen = ({ bitsData, timeData, arrayColumns, arrayRows }) => {
                 return (
                   <Led
                     key={`led_${i}${j}`}
-                    colour={bit === 1 ? colours["green"] : colours["grey"]}
+                    colour={bit === 1 ? colours["blue"] : colours["grey"]}
                     x={arrayRows[i]}
                     y={arrayColumns[j]}
                     r={r}
@@ -81,14 +145,15 @@ const Screen = ({ bitsData, timeData, arrayColumns, arrayRows }) => {
           );
         })}
         {/* time units */}
-        {timeData.map((digit, j) => {
+        {timeDigits.map((digit, j) => {
           return (
             <React.Fragment key={`digit_frag_${j}`}>
               <text
                 x={arrayRows[j]}
-                y={arrayColumns[timeData.length === 6 ? 3 : 6]}
-                baselineShift="-10px"
+                y={arrayColumns[timeDigits.length === 6 ? 3 : 6] + 18}
                 textAnchor="middle"
+                fill="#d1fff2"
+                className={"clock-class"}
               >
                 {digit}
               </text>
