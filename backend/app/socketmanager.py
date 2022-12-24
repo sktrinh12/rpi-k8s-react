7070794ea050198e5111fd37d13710ee16fe7cac
sktrinh12 <sktrinh12@gmail.com>
2022-12-24 02:46:17 -0800

    add socket class

---
+++ backend/app/socketmanager.py
@@ -0,0 +1,20 @@
+from fastapi import WebSocket, WebSocketDisconnect
+from typing import List
+
+class SocketManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_data(self, data: str, websocket: WebSocket):
+        await websocket.send_text(data)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
